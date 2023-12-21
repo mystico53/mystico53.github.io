ad6c1a8329d96a4c04f14b2175d896fca8bfd834
mystico53 <konkaiser@gmail.com>
2023-12-21 13:55:17 -0800

    added links

--- assets/js/snippets.js
+++ assets/js/snippets.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Initialize tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    // Initialize toasts
     const toastTrigger = document.getElementById('liveToastBtn');
     const toastLiveExample = document.getElementById('liveToast');
-
     if (toastTrigger) {
         toastTrigger.addEventListener('click', () => {
             const toast = new bootstrap.Toast(toastLiveExample);
             toast.show();
         });
     }
+
+    var youtubeModal = document.getElementById('youtubeModal')
+    youtubeModal.addEventListener('hide.bs.modal', function (event) {
+        var iframe = youtubeModal.querySelector('iframe');
+        var videoSrc = iframe.src;
+        iframe.src = '';
+        setTimeout(function () {
+            iframe.src = videoSrc;
+        }, 500);
+    });
 });
